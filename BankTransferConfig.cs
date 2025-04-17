4ab80fbdbee6dc1f7908a1290751f54d5c71be9b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 11:02:20 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300148
+{
+    public class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public class Transfer
+        {
+            public Transfer(int threshold, int low_fee, int high_fee)
+            {
+                this.threshold = threshold;
+                this.low_fee = low_fee;
+                this.high_fee = high_fee;
+            }
+
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+        
+        public List<string> methods { get; set; }
+        public class Confirmation
+        {
+            public Confirmation(string en, string id)
+            {
+                this.en = en;
+                this.id = id;
+            }
+
+            public string en { get; set; }
+            public string id { get; set; }
+        }
+        public Transfer transfer { get; set; }
+        public Confirmation confirmation { get; set; }
+          public BankTransferConfig() { }  
+        public BankTransferConfig(string lang, List<string> methods, Transfer transfer, Confirmation confirmation)
+        {
+            this.lang = lang;
+            this.methods = methods;
+            this.transfer = transfer;
+            this.confirmation = confirmation;
+        }
+    }
+    public class UIConfig
+    {
+        public BankTransferConfig config;
+        public const String filePath = "D:\\bank_transfer_config.json";
+        public UIConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+        public BankTransferConfig ReadConfigFile()
+        {
+            String jsonString = File.ReadAllText(filePath);
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true
+            };
+            BankTransferConfig json = JsonSerializer.Deserialize<BankTransferConfig>(jsonString, options);
+            return json;
+        }
+        public void SetDefault()
+        {
+            config = new BankTransferConfig();
+            config.lang = "en";
+            config.transfer = new BankTransferConfig.Transfer(25000000, 6500, 15000);
+            config.methods = new List<string>() { "RTO (real-time)", "SKN", "RTGS", "BI FAST" };
+            config.confirmation = new BankTransferConfig.Confirmation("yes", "ya");
+        }
+        public void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul8_103022300148;
+using static modul8_103022300148.BankTransferConfig;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        BankTransferConfig bankConfig = new BankTransferConfig();
+        UIConfig uIConfig = new UIConfig();
+        uIConfig.SetDefault();
+        uIConfig.ReadConfigFile();
+        Console.Write("Masukkan Bahasa en/id : ");
+        string bahasa = Console.ReadLine();
+        uIConfig.config.lang = bahasa;
+        if (uIConfig.config.lang == "en")
+        {
+            Console.Write("Please insert the amount of money to transfer : ");
+        }
+        else
+        {
+            Console.Write("Masukkan jumlah uang yang akan di - transfer : ");
+        }
+        int nilai = int.Parse(Console.ReadLine());
+        int total,fee;
+        if (nilai <= uIConfig.config.transfer.threshold) {
+            fee = uIConfig.config.transfer.low_fee;
+            total = nilai + fee;
+        }
+        else
+        {
+            fee = uIConfig.config.transfer.high_fee;
+            total = nilai + fee;
+        }
+        if (uIConfig.config.lang == "en")
+        {
+            Console.WriteLine("Transfer fee = " + fee  + " ,Total amount = " + total);
+        }
+        else
+        {
+            Console.WriteLine("Biaya transfer = " + fee + " ,Total biaya = " + total);
+        }
+        if (uIConfig.config.lang == "en")
+        {
+            Console.WriteLine("Select transfer method:");
+        }
+        else
+        {
+            Console.WriteLine("Pilih metode transfer:");
+        }
+        for (int i = 0; i < uIConfig.config.methods.Count; i++) { 
+            Console.WriteLine((i+ 1) + ". " + uIConfig.config.methods[i]);
+        }
+        int pilih = int.Parse(Console.ReadLine());
+        if (uIConfig.config.lang == "en")
+        {
+            Console.Write("Please type "+uIConfig.config.confirmation.en +" to confirm the transaction:");
+        }
+        else
+        {
+            Console.Write("Ketik "+uIConfig.config.confirmation.id+" untuk mengkonfirmasi transaksi:");
+        }
+        string input = Console.ReadLine();
+        if (uIConfig.config.lang == "en" && input == uIConfig.config.confirmation.en)
+        {
+            Console.Write("The transfer is completed");
+        }
+        else if (input == uIConfig.config.confirmation.id)
+        {
+            Console.Write("Proses transfer berhasil");
+        }
+        else if (uIConfig.config.lang == "en" && input != uIConfig.config.confirmation.en)
+        {
+            Console.Write("Transfer is cancelled");
+        }
+        else if (input != uIConfig.config.confirmation.id)
+        {
+            Console.WriteLine("Transfer dibatalkan");
+        }
+        uIConfig.WriteNewConfigFile();
+    }
+}
+
